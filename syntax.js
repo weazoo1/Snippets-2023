07a52804662b32a99f7d8ed06fef35efd929f62a
Jeraldy <119564168+weazoo1@users.noreply.github.com>
2023-01-25 21:31:12 -0800

    Add files via upload

---
+++ syntax.js
@@ -0,0 +1,26 @@
+// Reviewing JavaScript syntax.
+
+const THISWILLNOTCHANGE = {
+       //A constant is a variable that doesn't change in value. 
+    arr: [This,
+    is,
+    a,
+    test]
+        // An array creates a set of values within brackets ([]). Declare the array by typing out "arr:".
+        // Since the array is NESTED (inside) another variable, I must declare it by using "arr:" instead of "var arr ="
+    
+    }
+function Call() {
+    //Declare the function's name and parameters, then start a block ({}) that describes what the function will do.
+console.log("My first project.");
+}
+
+function Call();
+    //Call the function.
+
+var test = "test";
+var num = 50;
+
+var thisisa = "test", test = "test";
+console.log(num + 50);
+
